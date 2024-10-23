6d78cdcb1289fd393aab988a25da2611f7417425
waived <165493645+waived@users.noreply.github.com>
2024-10-23 13:30:16 -0400

    Add files via upload

---
+++ capture.php
@@ -0,0 +1,20 @@
+<?php
+
+// url redirect
+header('Location: www.example.com');
+
+// capture ip address
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// capture user agent
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+// capture time and date
+$dateTime = date('Y-m-d H:i:s'); // Format: YYYY-MM-DD HH:MM:SS
+
+// write data to file
+$fo = fopen("log.txt", "a");
+$fw = fwrite($fo, "$dateTime \t $ip \t $userAgent\n");
+fclose($fo);
+
+?>
